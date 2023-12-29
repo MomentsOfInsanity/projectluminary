9912b96bffb12dfc4e4293bcef0c37e57cb064b9
Raj Bedi <raj.j.bedi@gmail.com>
2023-12-28 22:51:19 -0500

    added ry series

---
+++ app/Controllers/Card.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Card extends BaseController
+{
+    public function RY()
+    {
+        return view('/personal/cards/ry_series');
+    }
+
+}
--- app/Views/personal/cards/ry_series.php
+++ app/Views/personal/cards/ry_series.php
@@ -99,7 +99,8 @@
                 <li><a href="https://youaremyeverything.net/" target="_blank">Cortex</a></li>
                 <li><a href="https://www.financeofthings.net/" target="_blank">Finance of Things</a></li>
                 <li><a href="https://www.youtube.com/@projectembers" target="_blank">ProjectEmbers(YT)</a></li>
-                <li><a href="https://www.youtube.com/@momentsofinsanity" target="_blank">Moments of Insanity(YT)</a></li>
+                <li><a href="https://www.youtube.com/@momentsofinsanity" target="_blank">Moments of Insanity(YT)</a>
+                </li>
 
 
             </ul>
@@ -193,7 +194,7 @@
                     <a href="/card/" class="navheader">Trading Cards</a>
                     <ul>
                         <li><a href="/card/ry">-RY Series</a></li>
-                        
+
 
                     </ul>
                 </li>
@@ -288,86 +289,41 @@
             </div> -->
             <main>
 
-                <div class="post">
-                    <h2><a href="/news/2023/28-December-2023.shtml" id="28-December-2023">Pinned: Building in Progress</a></h2>
-                    <p class="info"><span class="date">12-28-2023 09:00 PM EST</span> by <span class="user"><a
-                                href="mailto:raj.j.bedi@gmail.com">Asura</a></span>.</p>
-                    <p>This is a site for me to experiment and build on. Here is a weather API connected to my github. It will update automatically.<br>
-                        <b>Last Update:</b> 09:00 PM EST</p>
+                <!--
 
-                    <div class="subcat">
-                        <h3>WEATHER and DAY UPDATE</h3>
-                        <p class="title">Chatting with Raj</p>
-                        <p><a href="https://github.com/MomentsOfInsanity" target="_blank"><img src="https://raw.githubusercontent.com/MomentsOfInsanity/MomentsOfInsanity/main/chat.svg" width="300"
-                                alt="Weather chat" loading="lazy"></a>
-                        </p>
-                    </div>
+#######################################
+
+###############START Page #############
+
+#######################################
+
+-->
 
 
+                <p align="center">
+                    <font size="3"><b><u>X &amp; Y Series</u></b></font>
+                </p>
 
 
-                </div>
+                <p>The following card sets are the Pokémon Card sets released during the fifth generation period.
+                    Technically seperated into X &amp; Y sets, these sets cover the sixth generation cards. These focus
+                    upon the sixth generation Pokémon and brought several returning mechanics to the fold including <a
+                        href="/card/dex/extra/ex.shtml">Pokémon EX</a> and Mega Evolution.</p>
 
-
-                <div class="post">
-                    <h2><a href="/news/2023/28-December-2023.shtml" id="28-December-2023">Thursday: Pokémon Concierge -
-                            Release</a></h2>
-                    <p class="info"><span class="date">28-12-2023 08:00 GMT / 03:00 EST</span> by <span class="user"><a
-                                href="mailto:webmaster@serebii.net">Serebii</a></span>.</p>
-                    <p>This update will be amended throughout the day so be sure to check back. If you have any ideas
-                        for the site, be sure to send them in.<br>
-                        <b>Last Update:</b> 09:01 GMT</p>
-
-                    <div class="pics"><a href="/pokemonconcierge/"><img src="/pokemonconcierge/title2.jpg" width="300"
-                                alt="Pokémon Concierge" loading="lazy"></a></div>
-                    <div class="subcat">
-                        <h3>In The TV Department</h3>
-                        <p class="title">Pokémon Concierge</p>
-                        <p>The new TV show, Pokémon Concierge, is now live to watch on Netflix across the world. This
-                            series is a stop motion animated series following the exploits fo Haru as she goes to work
-                            at a Pokémon Resort and befriends many of the Pokémon she finds there. There are four
-                            episodes within the series ranging from 14 to 20 minutes each.<br>
-                            We're currently working through and cataloguing all details of all four episodes in our <a
-                                href="/pokemonconcierge">Pokémon Concierge</a> section so be sure to keep checking back
-                            for full details<br>
-                            Edit @ 09:01: We have updated the Pokémon Concierge section with details for every episode
-                            as well as what characters and Pokémon appear within
-                        </p>
-                    </div>
-
-
-
-
-                </div>
-
-                <!-- end_news -->
-                <div align="left">
-                    <p>Until Next Time, See Ya</p>
-                    <p></p>
-                    <div align="center"><img src="/anime/pokemon/251.gif" width="131" height="165" border="0"></div>
-                    <p></p>
-                </div>
-                
-
-                
-
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-
-
-                <div style="text-align:right;margin:1em;"><a href="/archive.shtml">Archived News</a></div>
+                <table class="tab" align="center">
+                    <tbody>
+                        <tr>
+                            <td class="fooevo">Set Name</td>
+                            <td class="fooevo">Number of Cards</td>
+                            <td class="fooevo">Logo</td>
+                        </tr>
+                        <tr>
+                            <td class="cen"><a href="/card/ry">R Y</a></td>
+                            <td class="cen">120</td>
+                            <td class="cen"><a href="/card/ry"><img src="/assets/media/personal/cards/ry/RYLogo.png" border="0"></a></td>
+                        </tr>
+                    </tbody>
+                </table>
 
             </main>
         </div><!-- END #content -->
