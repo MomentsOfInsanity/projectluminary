5a9721846269adddf401f0b1b7e6d1fbc5a60827
Raj Bedi <raj.j.bedi@gmail.com>
2024-01-03 11:29:32 -0500

    Update daily.php

--- app/Views/personal/exercise/daily.php
+++ app/Views/personal/exercise/daily.php
@@ -6,34 +6,6 @@
             <style>
                 @import url("https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap");
 
-:root {
-  --border-color: #144fc6;
-  --fill-color: #6ab3f8;
-}
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  background-color: #3494e4;
-  color: #fff;
-  font-family: "Montserrat", sans-serif;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 40px;
-}
-
-h1 {
-  margin: 10px 0 0;
-}
-
-h3 {
-  font-weight: 400;
-  margin: 10px 0;
-}
-
 .cup {
   background-color: #fff;
   border: 4px solid var(--border-color);
@@ -74,40 +46,9 @@ h3 {
   width: 280px;
 }
 
-.remained {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  flex: 1;
-  transition: 0.3s ease;
-}
 
-.remained span {
-  font-size: 20px;
-  font-weight: bold;
-}
 
-.remained small {
-  font-size: 12px;
-}
 
-.percentage {
-  background-color: var(--fill-color);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: bold;
-  font-size: 30px;
-  height: 0;
-  transition: 0.3s ease;
-}
-
-.text {
-  text-align: center;
-  margin: 0 0 5px;
-}
             </style>
     <main>
 
@@ -121,7 +62,7 @@ h3 {
                 <p class="title">Better start somewhere</p>
                 <p class="text">Select how many glasses of water that you have drank</p>
 <div class="cups">
-    <div class="cup cup-small">250 ml</div>
+    <div class="cup cup-small full">250 ml</div>
     <div class="cup cup-small">250 ml</div>
     <div class="cup cup-small">250 ml</div>
     <div class="cup cup-small">250 ml</div>
