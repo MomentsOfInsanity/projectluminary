6fcba3995bd36101ca840f96a8a5a2e13bb9c9b8
Raj Bedi <raj.j.bedi@gmail.com>
2024-01-03 11:24:17 -0500

    Update daily.php

--- app/Views/personal/exercise/daily.php
+++ app/Views/personal/exercise/daily.php
@@ -3,43 +3,133 @@
                 <div id="nn_lb1"></div>
                 <div id="nn_mobile_lb1"></div>
             </div> -->
+            <style>
+                @import url("https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap");
+
+:root {
+  --border-color: #144fc6;
+  --fill-color: #6ab3f8;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #3494e4;
+  color: #fff;
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 40px;
+}
+
+h1 {
+  margin: 10px 0 0;
+}
+
+h3 {
+  font-weight: 400;
+  margin: 10px 0;
+}
+
+.cup {
+  background-color: #fff;
+  border: 4px solid var(--border-color);
+  color: var(--border-color);
+  border-radius: 0 0 40px 40px;
+  height: 330px;
+  width: 150px;
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.cup.cup-small {
+  height: 95px;
+  width: 50px;
+  border-radius: 0 0 15px 15px;
+  background-color: rgba(255, 255, 255, 0.9);
+  cursor: pointer;
+  font-size: 14px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin: 5px;
+  transition: 0.3s ease;
+}
+
+.cup.cup-small.full {
+  background-color: var(--fill-color);
+  color: #fff;
+}
+
+.cups {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  width: 280px;
+}
+
+.remained {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  flex: 1;
+  transition: 0.3s ease;
+}
+
+.remained span {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.remained small {
+  font-size: 12px;
+}
+
+.percentage {
+  background-color: var(--fill-color);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  font-size: 30px;
+  height: 0;
+  transition: 0.3s ease;
+}
+
+.text {
+  text-align: center;
+  margin: 0 0 5px;
+}
+            </style>
     <main>
 
         <div class="post">
-            <h2><a href="/news/2023/28-December-2023.shtml" id="28-December-2023">Pinned: Building in Progress</a></h2>
-            <p class="info"><span class="date">12-28-2023 09:00 PM EST</span> by <span class="user"><a
-                        href="mailto:embersmoi@gmail.com">Asura</a></span>.</p>
-            <p>This is a site for me to experiment and build on. Here is a weather API connected to my github. It will
-                update automatically.<br>
-                <b>Last Update:</b> 09:00 PM EST</p>
-
-            <div class="subcat">
-                <h3>WEATHER and DAY UPDATE</h3>
-                <p class="title">Chatting with Raj</p>
-                <p><a href="https://github.com/MomentsOfInsanity" target="_blank"><img
-                            src="https://raw.githubusercontent.com/MomentsOfInsanity/MomentsOfInsanity/main/chat.svg"
-                            width="500" alt="Weather chat" loading="lazy"></a>
-                </p>
-            </div>
-
-
-
-
-        </div>
-
-        <div class="post">
-            <h2>Tuesday: Searching for Couches</h2>
-            <p class="info"><span class="date">01-02-2024</span> by <span class="user"><a
+            <h2>Wednesday: Daily Progress</h2>
+            <p class="info"><span class="date">01-03-2024</span> by <span class="user"><a
                         href="mailto:embersmoi@gmail.com">Asura</a></span>.</p>
 
             <div class="subcat">
-                <h3>Hanging out with...</h3>
-                <p class="title">Search for the couches.</p>
-                <p>Today was a interesting day. It was my first day back to work. It was a bit tiring but I got throught it. I went to Glenn's after work.
-                    With Joe and Glenn we went to go look for couches. We had spent a good amount of time in Raymore and Flanigans. It was interesting to go through
-                    but we could not find a couch to meet the requirements. We then went to Ashley's and luckily we found a nice couch that Glenn might decide on later.
-                    I decided I am going to make more daily posts as well. Anything significant in my day. 
-                </p>
+                <h3>Starting the journey</h3>
+                <p class="title">Better start somewhere</p>
+                <p class="text">Select how many glasses of water that you have drank</p>
+<div class="cups">
+    <div class="cup cup-small">250 ml</div>
+    <div class="cup cup-small">250 ml</div>
+    <div class="cup cup-small">250 ml</div>
+    <div class="cup cup-small">250 ml</div>
+    <div class="cup cup-small">250 ml</div>
+    <div class="cup cup-small">250 ml</div>
+    <div class="cup cup-small">250 ml</div>
+    <div class="cup cup-small">250 ml</div>
+</div>
             </div>
 
 
@@ -49,125 +139,6 @@
 
         </div>
 
-        <!-- end_news -->
-        <div align="left">
-            <p>Until Next Time, See Ya</p>
-            <p></p>
-        </div>
-
-        <div class="post">
-            <h2>Monday: Queens day</h2>
-            <p class="info"><span class="date">01-01-2024</span> by <span class="user"><a
-                        href="mailto:embersmoi@gmail.com">Asura</a></span>.</p>
-
-            <div class="subcat">
-                <h3>Beginning of the month</h3>
-                <p class="title">Return to Queens.</p>
-                <p>Nothing much happened today. Got home from the New Years party. Then I went to Queens to pick up rent and garbage. Then I came home.
-                </p>
-            </div>
-
-
-
-
-
-
-        </div>
-
-        <!-- end_news -->
-        <div align="left">
-            <p>Until Next Time, See Ya</p>
-            <p></p>
-        </div>
-
-        <div class="post">
-            <h2>Sunday: HAPPY NEW YEAR & HAPPY BIRTHDAY!</h2>
-            <p class="info"><span class="date">12-31-2023</span> by <span class="user"><a
-                        href="mailto:embersmoi@gmail.com">Asura</a></span>.</p>
-
-            <div class="pics"><a href="/assets/media/personal/NYE2023.jpg"><img src="/assets/media/personal/NYE2023.jpg" width="300"
-            alt="Group pic" loading="lazy"></a></div>
-
-            <div class="subcat">
-                <h3>I AM 26!</h3>
-                <p class="title">Happy New Year!</p>
-                <p>HAPPY BIRTHDAY! HAPPY NEW YEAR! I am 26 years old now. I am getting old. I am getting closer to 30. I am getting closer to death. I am getting closer to the end. 
-                    <br>
-                    That is funny. I have a auto suggest and man that auto suggest is dark. This was the first time I got to celebrate my birthday with the stunningly gorgeous Yu. 
-                    I got to meet a lot of old friends that I have not seen in ages like Asha and Rithika. I made some new friends as well! This was the first party I decided to go all out.
-                    I drank... I am saying 14 shots but it was a mix of vodka, tequila, malt, wine etc. It didn't go as planned as I forgot alcohol toxicity is a thing so I threw up.
-                    Luckily I threw it up so I was able to skip being drunk and hung over ~ I was able to enjoy the rest of the night. I really love yu. She took care of me and everyone
-                    at this part was so nice and friendly. Did I mention how beautiful she looked? 
-                    <br>
-                    <br>
-                    I also got cake and they spread it on my face.
-                </p>
-            </div>
-
-            <div class="pics"><a href="/assets/media/personal/cakeface.jpg"><img src="/assets/media/personal/cakeface.jpg" width="300"
-            alt="Cake on my face" loading="lazy"></a></div>
-
-
-
-
-        </div>
-
-        <!-- end_news -->
-        <div align="left">
-            <p>Until Next Time, See Ya</p>
-            <p></p>
-        </div>
-
-
-
-        <div class="post">
-            <h2>Friday: Boys Hangout - Firepit</h2>
-            <p class="info"><span class="date">12-29-2023 / 09:40 EST</span> by <span class="user"><a
-                        href="mailto:embersmoi@gmail.com">Asura</a></span>.</p>
-
-            <div class="pics"><a href="/assets/media/personal/boys/fireplace.jpg"><img src="/assets/media/personal/boys/fireplace.jpg" width="300"
-            alt="Boys fireplace" loading="lazy"></a></div>
-
-            <div class="subcat">
-                <h3>Hanging Out With...</h3>
-                <p class="title">Boys Hangout</p>
-                <p>Today was the first time we hanged out using a fire pit at glenn and phills house. I burned couple sticks and marshmallows.
-                    I kebobed a pizza and made it nice and toasty. It was a chill experience. Joe phill and glenn was there. The fire was nice but was not warm enough
-                    to fully heat up everywhere. It was great hanging with the boys. Our feet got nice and toasty!
-                </p>
-            </div>
-
-
-
-
-        </div>
-
-        <!-- end_news -->
-        <div align="left">
-            <p>Until Next Time, See Ya</p>
-            <p></p>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <div style="text-align:right;margin:1em;"><a href="/archive.shtml">Archived News</a></div>
 
     </main>
 </div><!-- END #content -->
