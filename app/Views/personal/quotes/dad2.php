8a6a0c11064ca98171991dd0d0a2ef2a735add68
Raj Bedi <raj.j.bedi@gmail.com>
2024-01-19 23:00:10 -0500

    started dad 2

--- app/Views/personal/quotes/dad2.php
+++ app/Views/personal/quotes/dad2.php
@@ -6,16 +6,68 @@
     <main>
 
         <div class="post">
-            <h2><a href="/news/2023/28-December-2023.shtml" id="28-December-2023">Quote of the Week: Jan 1st - Jan 6th 2024</a></h2>
-            <p class="info"><span class="date">01-01-2024</span> by <span class="user"><a
+            <h2><a >PAGE 1</a></h2>
+            <p class="info"><span class="date">01-05-2024</span> by <span class="user"><a
                         href="mailto:embersmoi@gmail.com">Asura</a></span>.</p>
             
 
             <div class="subcat">
-                <h3>WEEK 1</h3>
-                <p class="title">QOTW</p>
-                <p>“A year from now you may wish you had started today.” — Karen Lamb
+                <h3>MY JOURNAL(1)</h3>
+                <p class="title">For My Son, Raj Bedi, Love Daddy</p>
+                
+                <br>
+                <p>
+                    <u>JAI SHREE KRISHNA</u><br>
+                    <ul>
+                        <li><u>SHIRDI WALE SAI BABA</u></li>
+                        <li><u>MAA VAISHNO DEVI</u></li>
+                        <li><u>MAALIK PRABHU</u></li>
+                        <li><u>SHIVA - PARVATI</u></li>
+                        <li><u>SATNAM - WAHEGURU</u></li>
+                        <li><u>RAM - SITA - LAXMAN - HANUMAN</u></li>
+                        <li><u>FATHER IN HEAVEN</u></li>
+                        <li><u>DASHMESH GURUS</u></li>
+                        <li><u>GURU GRANTH SAHIB</u></li>
+                        <li><u>JAI MATA DI</u></li>
+                        <li><u>HAJI ALI BABA</u></li>
+                        <li><u>BUDDHA</u></li>
+                        <li><u>YAHVEN</u></li>
+                        <li><u>ALLAH</u></li>
+                        <li><u>JESUS</u></li>
+                        <li><u>NAVGRAH</u></li>
+                        <li><u>"GOD"</u></li>
+                        <li><u>SHANI</u></li>
+                        <li><u>MOSES</u></li>
+                        <li><u>JAI GANPATI</u></li>
+                        <li><u>JAI MAHALAXMI</u></li>
+                        <li><u>DAATA</u></li>
+                        <li><u>DEVIS - DEVATAS</u></li>
+                        <li><u>SAINTS</u></li>
+                        <li><u>PROHPHET MOHAMMED</u></li>
+                        <li><u>GURUS</u></li>
+                        <li><u>MAHAVEER</u></li>
+                        <li><u>BHAGWAN SATYA SAI BABA</u></li>
+                        <li><u>HOLY SPIRIT</u></li>
+                        <li><u>SHREE VENKATESHWARA SWAYAMBHU</u></li>
+                        <li><u>PADMAVATI</u></li>
+                        <li><u>MAA SHERAWALI</u></li>
+                        <li><u>MAA DURGA</u></li>
+                        <li><u>NEELI CHATRIWALE</u></li>
+                        <li><u>UPARWALE</u></li>
+                        <li><u>HOLY GHOST</u></li>
+                        <li><u>MAA MAHAKAALS</u></li>
+                        <li><u>MAA MAHASARASWATE</u></li>
+                        <li><u>SURYA</u></li>
+                        <li><u>KAZRAT KHWAJA MOINUDIN CHISTI</u></li>
+                        <li><u>HAZRAT KHWAJA NIZAMUDIN</u></li>
+                        <li><u>TIRTH GURU SHREE PUSHKAR RAJ - BRAHMA</u></li>
+                    </ul>
                 </p>
+                <br>
+                <br>
+                <p><b>Update:</b> These books are interesting. Much of it wont make sense to me. This was just a page of names religiously I think.
+                    Also he writes everything in caps. I will also type the same in caps and post the pictues of the books later. Maybe one day i'll
+                    get these pages translated. It is a mixture of non-english and english pages.</p>
             </div>
 
 
@@ -24,6 +76,8 @@
         </div>
 
 
+        
+        
 
     </main>
 </div><!-- END #content -->
