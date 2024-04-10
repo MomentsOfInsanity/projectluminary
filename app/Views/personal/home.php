f88bc960ef399348c4ebcc2439bd549dd2a2313e
Raj Bedi <raj.j.bedi@gmail.com>
2024-04-10 12:42:57 -0400

    added css

--- app/Views/personal/home.php
+++ app/Views/personal/home.php
@@ -1985,17 +1985,17 @@
 
     function showSlides2(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides2");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex2 = 1}
-    if (n < 1) {slideIndex2 = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    let slides2 = document.getElementsByClassName("mySlides2");
+    let dots2 = document.getElementsByClassName("dot");
+    if (n > slides2.length) {slideIndex2 = 1}
+    if (n < 1) {slideIndex2 = slides2.length}
+    for (i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < dots2.length; i++) {
+        dots2[i].className = dots2[i].className.replace(" active", "");
     }
-    slides[slideIndex2-1].style.display = "block";
-    dots[slideIndex2-1].className += " active";
+    slides2[slideIndex2-1].style.display = "block";
+    dots2[slideIndex2-1].className += " active";
     }
 </script>
