ee45b002161821754fdbb3fabfa6b37c3d18c933
Raj Bedi <raj.j.bedi@gmail.com>
2025-01-13 09:42:08 -0500

    removed unnecessary from main

--- app/Views/personal/archive/home_2024.php
+++ app/Views/personal/archive/home_2024.php
@@ -6,25 +6,6 @@
     <main>
 
         <div class="post">
-            <h2><a href="/news/2023/28-December-2023.shtml" id="28-December-2023">Pinned: Building in Progress</a></h2>
-            <p class="info"><span class="date">01-22-2025</span> by <span class="user"><a
-                        href="mailto:embersmoi@gmail.com">Asura</a></span>.</p>
-            <p>This is a site for me to experiment and build on. Here is a weather API connected to my github. It will
-                update automatically.<br>
-                <b>Last Update:</b> 09:00 PM EST</p>
-
-            <div class="subcat">
-                <h3>WEATHER and DAY UPDATE</h3>
-                <p class="title">Chatting with Raj</p>
-                <p><a href="https://github.com/MomentsOfInsanity" target="_blank"><img
-                            src="https://raw.githubusercontent.com/MomentsOfInsanity/MomentsOfInsanity/main/chat.svg"
-                            width="500" alt="Weather chat" loading="lazy"></a>
-                </p>
-            </div>
-
-        </div>
-
-        <div class="post">
             <h2>Wednesday: The Moments Worth Remembering!</h2>
             <p class="info"><span class="date">04-11-2024</span> by <span class="user"><a href="mailto:embersmoi@gmail.com">Asura</a></span></p>
 
