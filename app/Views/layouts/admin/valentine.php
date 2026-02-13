5f9ddb3235ca929039c2137b30aca000e775951c
Raj Bedi <71458567+MomentsOfInsanity@users.noreply.github.com>
2026-02-13 15:24:05 -0500

    Update valentine.php

--- app/Views/layouts/admin/valentine.php
+++ app/Views/layouts/admin/valentine.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" id="viewport-meta">
-    <title>4 u my love!!! :3</title>
+    <title>All the love</title>
     <meta http-equiv="imagetoolbar" content="no">
 </head>
 
